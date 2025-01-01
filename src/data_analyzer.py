874f9515abc4a3e96f267c3c7de12c3060c458bf
AchourSimoud <achoursimoud@gmail.com>
2025-01-01 01:33:05 +0100

    Analyse des données

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 class DataAnalyzer:
     def __init__(self, data : pd.DataFrame):
         self.data = data
@@ -21,7 +22,9 @@ class DataAnalyzer:
         return missing_counts
     
     def product_id_consistency(self):
-        """Vérifie la consistance entre StockCode et Description"""
+        """
+        Vérifie la consistance entre StockCode et Description
+        """
         # Grouper par StockCode et vérifier les descriptions uniques
         product_desc = self.data.groupby('StockCode')['Description'].agg(['nunique', 'unique'])
             
@@ -39,3 +42,30 @@ class DataAnalyzer:
             })
                 
         return pd.DataFrame(detailed_inconsistencies)
+    
+    def country_distrib(self):
+        """
+        Affiche et retourne le nombre de transactions par pays
+        """
+        transactions = self.data ['Country'].value_counts()
+        plt.figure(figsize=(10, 6))
+        transactions.plot(kind='barh', color='skyblue', edgecolor='black')
+        plt.title("Nombre de transactions par pays", fontsize=16)
+        plt.xlabel("Pays", fontsize=14)
+        plt.ylabel("Nombre de transactions", fontsize=14)
+        plt.xticks(rotation=45, fontsize=12)
+        plt.xticks(rotation=45, fontsize=12)
+        plt.grid(axis='x', linestyle='--', alpha=0.7)
+        plt.show()
+
+    def stat_analitics(self):
+        """
+        Affiche la distribution des quantités et prix unitaires
+        """
+        # Analyse de la distribution des données numériques
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
+        sns.boxplot(y=self.data['Quantity'], ax=ax1)
+        sns.boxplot(y=self.data['UnitPrice'], ax=ax2)
+        ax1.set_title('Distribution Quantity')
+        ax2.set_title('Distribution UnitPrice')
+        plt.show()
